53b304a03c9f33e913a7e5e3ffe26f84795396c7
Marcelo Pasut <pasutmarcelo@gmail.com>
2019-10-09 14:14:36 -0300

    agrego register view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppTopBarComponent } from './app-top-bar/app-top-bar.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppTopBarComponent
+    AppTopBarComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
